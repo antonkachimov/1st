f5390f704ce34b9cc6725af773a4166eefe20844
Антон-PC\Антон <antonkachimov@gmail.com>
2020-07-02 23:55:08 +0400

    Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,9 @@
 package com.company;
 
 public class Main {
-
+	double d2 = 0.0f;
     public static void main(String[] args) {
-	    String hello = "Hello";
+    	String hello = "Hello";
 	    String world = "world";
 	    System.out.println(hello + " " + world + "!!!");
     }
